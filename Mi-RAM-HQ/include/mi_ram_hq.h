a1ebe05d7ea4e8638d1d4103ce59fb8e23a162d4
maribelAldana <maribel.aldana.sandoval@gmail.com>
2021-05-06 01:37:27 -0300

    Agrego estructura de mi-ram

---
+++ Mi-RAM-HQ/include/mi_ram_hq.h
@@ -0,0 +1,7 @@
+#ifndef MI_RAM_HQ_H
+#define MI_RAM_HQ_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#endif
---
+++ Mi-RAM-HQ/src/mi_ram_hq.c
@@ -0,0 +1,8 @@
+#include "mi_ram_hq.h"
+
+int main(){
+
+    printf("hola\n");
+
+    return 1;
+}
