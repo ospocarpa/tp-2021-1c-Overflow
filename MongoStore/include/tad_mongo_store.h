8d2e510efb13a822dce358e53d02c04464e92f13
matiasflores94 <matias.flores.94@gmail.com>
2021-05-08 20:54:08 -0300

    Agrego base de server, basado en el server RamHQ

--- MongoStore/include/tad_mongo_store.h
+++ MongoStore/include/tad_mongo_store.h
@@ -1,8 +1,32 @@
-#ifndef DISCORDIADOR_H
-#define DISCORDIADOR_H
+#ifndef TAD_MONGO_STORE_H
+#define TAD_MONGO_STORE_H
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <commons/log.h>
+#include <commons/collections/list.h>
+
+typedef struct
+{
+    char* punto_montaje; 
+    t_list posiciones_sabotaje; //Lista [Posiciones]
+} Mongo_Store;
+
+typedef struct 
+{
+    __uint32_t block_size;
+    __uint32_t blocks;
+    _Bool bitmap;
+} SuperBloque;
+
+typedef struct 
+{
+    __uint32_t size;
+    __uint32_t block_count;
+    t_list blocks;
+    char caracter_llenado;
+    char* md5_archivo;
+} File;
+
 
 #endif
---
+++ MongoStore/src/read_config_mongo_store.c
@@ -0,0 +1,39 @@
+#include "read_config_mongo_store.h"
+
+static t_config *config = NULL;
+
+void cfg_create(char *path_config)
+{
+    if (config == NULL)
+        config = config_create(path_config);
+}
+
+char * get_punto_de_montaje()
+{
+    return config_get_string_value(config, "PUNTO_MONTAJE");
+}
+
+char* get_ip()
+{
+    return "127.0.0.1";
+}
+
+char* get_puerto()
+{
+    return config_get_string_value(config, "PUERTO");
+}
+
+int get_tiempo_sincronizacion()
+{
+    return config_get_int_value(config, "TIEMPO_SINCRONIZACION");
+}
+
+t_list* get_posiciones_sabotaje()
+{
+    return config_get_array_value(config, "POSICIONES_SABOTAJE");
+}
+
+void cfg_free()
+{
+    config_destroy(config);
+}
