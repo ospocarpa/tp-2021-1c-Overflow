294b3387acd40c441491a731a5dfb85f9313c033
achoquemamani <ariel0choque0mamani@gmail.com>
2021-05-09 16:45:12 -0300

    Se implementa hola mundo en mongo store

--- MongoStore/src/mongo_store.c
+++ MongoStore/src/mongo_store.c
@@ -2,23 +2,24 @@
 
 int main()
 {
+    printf("Hola mundo\n");
 
-    t_config* config = config_create("cfg/mongoStore.config");
+    //t_config* config = config_create("cfg/mongoStore.config");
 
-    t_log* log = iniciar_logger("cfg/mongoStore.log", "MONGO_STORE");
-    log_info(log, "Iniciando módulo i-Mongo-Store");
+    //t_log* log = iniciar_logger("cfg/mongoStore.log", "MONGO_STORE");
+    //log_info(log, "Iniciando módulo i-Mongo-Store");
 
-    char* ip = get_ip();
-    char *puerto = get_puerto();
+    //char* ip = get_ip();
+    //char *puerto = get_puerto();
     
-    log_info(log, "IP: %s", ip);
-    log_info(log, "PUERTO: %d", puerto);
+    //log_info(log, "IP: %s", ip);
+    //log_info(log, "PUERTO: %d", puerto);
 
-    server_mongo_store_iniciar(puerto, ip, NULL);
+    //server_mongo_store_iniciar(puerto, ip, NULL);
 
     // Libero el log y config al final
-    borrar_log(log);
-    config_destroy(config);
+    //borrar_log(log);
+    //config_destroy(config);
 
     return 1;
 }
