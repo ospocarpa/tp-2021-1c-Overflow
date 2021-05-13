b5ff0ccfe389393dc567000c9cc00cb0fdd22ba6
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-05-13 03:20:02 -0300

    posible cambio de mostrar_consola()

--- Discordiador/src/consola.c
+++ Discordiador/src/consola.c
@@ -1,53 +1,21 @@
 #include "consola.h"
-
+//inline
 void mostrar_consola()
 {
-    int var = 0;
-    char *accion = string_new();
+    printf(
+        "\n--------------------\n"
+        "BIENVENIDO A LA CONSOLA DEL DISCORDIADOR\n"
+        "Lista de comandos: \n"
+        "INICIAR_PATOTA [Cantidad de tripulantes] [Ruta] [Coordenadas]\n  "
+        "LISTAR_TRIPULANTES\n"
+        "EXPULSAR_TRIPULANTE [Numero de tripulante]\n"
+        "INICIAR_PLANIFICACION\n"
+        "PAUSAR_PLANIFICACION\n"
+        "OBTENER_BITACORA\n"
+        " SALIR\n"
 
-    while (var != 7)
-    {
-        printf("------------------------------------------------------\n");
-        printf("Ingrese un numero acorde a la instruccion a realizar \n");
-        printf("1-Iniciar Patota \n");
-        printf("2-Listar Tripulante \n");
-        printf("3-Expulsar Tripulante \n");
-        printf("4-Iniciar Planificacion \n");
-        printf("5-Pausar Planificacion \n");
-        printf("6-Obtener Bitacora \n");
-        printf("7-Salir \n");
-        printf("-----------------------------------------------------\n");
-        scanf("%d", &var);
-        printf("Ingrese instruccion: ");
-        scanf("%s", accion);
-        switch (var)
-        {
-        case 1:
-            /*char* cant_patotas = string_new();
-                char* path_tarea = string_new();
-                scanf("%d", &var);
-                scanf("%d", &var);
-                scanf("%d", &var);
-                scanf("%d", &var);
-                scanf("%d", &var);
-                printf("Parametros: %s\n", comando);*/
-            break;
-        case 2:;
-            break;
-        case 3:;
-            break;
-        case 4:;
-            break;
-        case 5:;
-            break;
-        case 6:;
-            break;
-        case 7:
-            printf("Fin de consola\n");
-            break;
-        }
-        system("clear");
-    }
+    );
+    //system("clear");
 }
 
 void liberar_puntero_doble(char **puntero_doble)
