f0e1e5fe1df127b3c08bb396d7b60837f1e3018b
achoquemamani <ariel0choque0mamani@gmail.com>
2021-05-25 13:58:48 -0300

    Se inicia cración de file system

--- MongoStore/include/controller.h
+++ MongoStore/include/controller.h
@@ -3,8 +3,14 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <global.h>
+#include <config_utils.h>
 
     void method_sigusr1();
     void init_mongo_store();
+    void create_punto_montaje(t_config_mongo_store* config_mongo_store);
+    void create_super_bloque(t_config_mongo_store* config_mongo_store);
+    void create_files(t_config_mongo_store* config_mongo_store);
+    void create_bitacoras(t_config_mongo_store* config_mongo_store);
 
 #endif
--- MongoStore/src/controller.c
+++ MongoStore/src/controller.c
@@ -6,5 +6,13 @@ void method_sigusr1(){
 
 void init_mongo_store(){
     //Inicialización del file system
-    
+    create_punto_montaje(t_config_mongo_store* config_mongo_store){}
+    create_super_bloque(t_config_mongo_store* config_mongo_store){}
+    create_files(t_config_mongo_store* config_mongo_store){}
+    create_bitacoras(t_config_mongo_store* config_mongo_store){}    
 }
+
+void create_punto_montaje(t_config_mongo_store* config_mongo_store){}
+void create_super_bloque(t_config_mongo_store* config_mongo_store){}
+void create_files(t_config_mongo_store* config_mongo_store){}
+void create_bitacoras(t_config_mongo_store* config_mongo_store){}
