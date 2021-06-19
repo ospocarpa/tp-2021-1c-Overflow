49b93738f9031e568036823a4b4bb0af089070e2
maribelAldana <maribel.aldana.sandoval@gmail.com>
2021-06-19 03:56:39 -0300

    Agrego funcion que recibe y responde los mensajes que llegan

--- Mi-RAM-HQ/src/server.c
+++ Mi-RAM-HQ/src/server.c
@@ -1,18 +1,67 @@
 #include "server.h"
 
-static void *ejecutar_operacion(int client)
+static void *ejecutar_operacion(int tripulante)
 {
-	char * mensaje = malloc(4);
-	//aca van los mensajes que envia discordiador
+	t_package paquete;
+	t_package paquete_res;
+	/* INICIAR PATOTA */
+	t_iniciar_patota init_patota;
+	/* LISTAR TRIPULANTES */
+	t_listar_tripulantes tripulantes;
+	/* EXPULSAR TRIPULANTE */
+	t_expulsar_tripulante ex_tripulante;
+	/* INFORMAR TAREA */
+	t_short_info_tripulante tripulante_info;
+	t_info_tarea tarea;
+	/* INFORMAR POSICION */
 
-	//ejemplo, luego eliminar
-	int res = recv(client, mensaje, 4, 0);
-	mensaje[4]='\0';
-	logger_info("Mensaje: %s", mensaje);
+	paquete = recibir_mensaje(tripulante);
 
-	//
+	switch (paquete.cod_operacion)
+	{
+	case INICIAR_PATOTA:
+		logger_info("INICIAR PATOTA");
+		/* interpreto el contenido del mensaje */
+		init_patota = des_cod_iniciar_patota(paquete);
+		//mi funcion iniciar patota
+		break;
 
-	free(mensaje);
+	case LISTAR_TRIPULANTES:
+		logger_info("LISTAR TRIPULANTES");
+		/* no deserializo el contenido del mensaje porque me envia la operacion */
+		//tripulantes = mi funcion listar tripulantes
+		/* serializo la respuesta al tripulante */
+		paquete_res = ser_res_listar_tripulantes(tripulantes);
+		/* envio respuesta */
+		sendMessage(paquete_res, tripulante);
+		break;
+
+	case EXPULSAR_TRIPULANTE:
+		logger_info("EXPULSAR TRIPULANTES");
+		/* interpreto el contenido del mensaje */
+		ex_tripulante = des_cod_expulsar_tripulante(paquete);
+		//mi funcion expulsar tripulante
+		break;
+
+	case INFORMAR_TAREA_TRIPULANTE:
+		logger_info("INFORMAR TARAE");
+		/* interpreto el contenido del mensaje */
+		tripulante_info = des_cod_informar_tarea_tripulante(paquete);
+		//tarea = ; mi funcion que devuelve una tarea
+		/* serializo la respuesta al tripulante */
+		paquete_res = ser_res_informar_tarea_tripulante(tarea);
+		/* envio respuesta */
+		sendMessage(paquete_res, tripulante);
+		break;
+
+	case INFORMAR_POSICION_TRIPULANTE:
+		/* code */
+		break;
+	
+	default:
+		logger_error("No existe operacion");
+		break;
+	}
 }
 
 void server_mi_ram_iniciar(int puerto)
