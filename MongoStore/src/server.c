da1164094c798154f3ef8f6a9e0cc71996d7cd03
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-06-21 01:27:59 -0300

    minicomit

--- MongoStore/src/server.c
+++ MongoStore/src/server.c
@@ -5,23 +5,23 @@ void *recepcionMensaje(t_paquete *paquete, int cliente_fd, t_log *logger)
     log_info(logger, "recibo algo %d", paquete->codigo_operacion);
     //Deserializacion
     t_paquete *new_paquete;
-    Tarea* tarea2;
+    Tarea *tarea2;
     switch (paquete->codigo_operacion)
     {
-        case 0:
-            exit;
+    case 0:
+        exit;
         break;
-        case TAREA: 
-            tarea2 = deserializar_I_TAREA(paquete);
-            mostrarTarea(tarea2);
-            break;
-        case 56: //Inicio de fsck 
-            init_protocolo_fsck();
-            break;
-        case 4: //Preguntar archivo y devolver 
-            break;
-        case 5: //Preguntar bitacora y devolver 
-            break;
+    case TAREA:
+        tarea2 = deserializar_I_TAREA(paquete);
+        mostrarTarea(tarea2);
+        break;
+    case 56: //Inicio de fsck
+        init_protocolo_fsck();
+        break;
+    case 4: //Preguntar archivo y devolver
+        break;
+    case 5: //Preguntar bitacora y devolver
+        break;
     }
 
     return NULL;
@@ -31,12 +31,12 @@ void ejecutar_operacion(int cliente_fd)
 {
     while (1)
     {
-        t_paquete *paquete = recibir_mensaje(cliente_fd);
-        if (paquete->codigo_operacion > 0 && paquete->codigo_operacion < 100)
+        t_package paquete = recibir_mensaje(cliente_fd);
+        if (paquete.cod_operacion > 0 && paquete.cod_operacion < 100)
         {
 
-            recepcionMensaje(paquete, cliente_fd, logger);
-            if (paquete->codigo_operacion == 0)
+            recepcionMensaje(&paquete, cliente_fd, logger);
+            if (paquete.cod_operacion == 0)
                 exit;
         }
         else
@@ -48,30 +48,30 @@ void ejecutar_operacion(int cliente_fd)
 
 void server_mongo_store_iniciar(int puerto)
 {
-	int socket_server;
-	int socket_client_tripulante;
-	pthread_t hilo_client_tripulante;
+    int socket_server;
+    int socket_client_tripulante;
+    pthread_t hilo_client_tripulante;
 
-	log_info(logger, "Iniciar servidor i-Mongo-Store");
-	socket_server = iniciar_servidor(puerto);
+    log_info(logger, "Iniciar servidor i-Mongo-Store");
+    socket_server = iniciar_servidor(puerto);
 
-	if(socket_server < 0)
-	{
-		perror("Server i-Mongo-Store no creado:");
-	}
+    if (socket_server < 0)
+    {
+        perror("Server i-Mongo-Store no creado:");
+    }
 
-	log_info(logger, "Server i-Mongo-Store N° [%d] esperando clientes", socket_server);
-	while (1)
-	{
-		socket_client_tripulante = esperar_cliente(socket_server);
+    log_info(logger, "Server i-Mongo-Store N° [%d] esperando clientes", socket_server);
+    while (1)
+    {
+        socket_client_tripulante = esperar_cliente(socket_server);
 
-		if (socket_client_tripulante != -1)
-		{
-			log_info(logger, "Se conecto el tripulante [%d]", socket_client_tripulante);
-			pthread_create(&hilo_client_tripulante, NULL, (void *)ejecutar_operacion, (void *)socket_client_tripulante);
-			pthread_detach(hilo_client_tripulante);
-		}
-	}
+        if (socket_client_tripulante != -1)
+        {
+            log_info(logger, "Se conecto el tripulante [%d]", socket_client_tripulante);
+            pthread_create(&hilo_client_tripulante, NULL, (void *)ejecutar_operacion, (void *)socket_client_tripulante);
+            pthread_detach(hilo_client_tripulante);
+        }
+    }
 
-	liberar_conexion(socket_server);
+    liberar_conexion(socket_server);
 }
