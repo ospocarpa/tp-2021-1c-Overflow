0d9442a7ab0f52691a2fdb5dfca32b1a344361a8
briancol07 <briancol07@gmail.com>
2021-07-17 21:26:23 -0300

    se agrega archivo seg_informar_estado(.h,.c)

---
+++ Mi-RAM-HQ/include/seg_informar_estado.h
@@ -0,0 +1,16 @@
+#ifndef SEG_INFORMAR_ESTADO_H
+#define SEG_INFORMAR_ESTADO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <commons/collections/list.h>
+#include "tad_mi_ram_hq.h"
+#include "log_utils.h"
+#include <string.h>
+
+
+void filtrar_pid(uint32_t pid , t_list * lista);
+
+
+#endif
---
+++ Mi-RAM-HQ/src/seg_informar_estado.c
@@ -0,0 +1,14 @@
+#include "seg_informar_estado.h"
+
+
+// primera lista list_tabla_segmentos
+//t_tabla_segmentos
+//void *list_find(t_list *, bool(*closure)(void*));
+
+void filtrar_pid(uint32_t pid, t_lista * lista){
+    bool _esIgual(list_tabla_sementos){
+        return 
+    }
+
+
+}
