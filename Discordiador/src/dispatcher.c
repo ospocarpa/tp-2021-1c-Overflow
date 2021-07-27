998a7c77d754602a516c337dc89054917263630e
achoquemamani <ariel0choque0mamani@gmail.com>
2021-07-27 00:59:36 -0300

    Se realizan correcciones de discordiador

--- Discordiador/src/dispatcher.c
+++ Discordiador/src/dispatcher.c
@@ -2,7 +2,9 @@
 
 
 void init_dispatcher(){
+    //printf("Inicio\n");
     while(1){
+        //printf("Ejecución\n");
         sem_wait(&listos);
         Tripulante* tripulante_seleccionado = busqueda_algoritmo();
         sem_wait(&grado_multiprocesamiento);
@@ -13,8 +15,10 @@ void init_dispatcher(){
 Tripulante* busqueda_algoritmo(){
     //printf("Config dispatcher: %d\n", config->ALGORITMO);
     //Retornar la cola de listos
+
     int indice = list_size(lista_READY)-1;
-    list_remove(lista_READY, indice);
     Tripulante* tripulante = list_get(lista_READY, indice);
+    list_remove(lista_READY, indice);
+    //printf("ID: %d\n", tripulante->id);
     return tripulante;
 }
