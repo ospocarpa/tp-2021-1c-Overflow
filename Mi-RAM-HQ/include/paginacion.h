c65e0498a74c2ce991edb32b30d880b9232e89a0
achoquemamani <ariel0choque0mamani@gmail.com>
2021-08-02 11:59:31 -0300

    Se crean estructuras necesarias

--- Mi-RAM-HQ/include/paginacion.h
+++ Mi-RAM-HQ/include/paginacion.h
@@ -4,7 +4,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/string.h> 
+#include <commons/bitarray.h>
 #include <commons/collections/list.h>
+
+    t_list * list_tablas_paginacion;        //[t_table_page]
+    t_list * tablas_pag_libre;         //?
+    t_bitarray* bitmap_memoria_real;
+    t_bitarray* bitmap_memoria_virtual;
+
+    //Estructura
+    typedef struct{
+        t_list* pages;                      //[t_page]
+        int cant_caracteres_tarea;
+        int cant_tripulantes;
+        int patota_id;                      //Es el conjunto de bloques}
+    } t_table_page;
+
+    typedef struct{
+        int page;
+        int frame;
+        bool presencia;
+        bool uso;
+        char* timestamp;
+    } t_page;
+
+    typedef struct{
+        void* stream;
+        int tamanio;
+        int cantTripulantes;
+    } t_page_auxiliar;
 
 void dump_paginacion();
 
--- Mi-RAM-HQ/src/paginacion.c
+++ Mi-RAM-HQ/src/paginacion.c
@@ -1,9 +1,5 @@
 #include "paginacion.h"
 
- static t_list * list_tablas_paginacion;
- static t_list * tablas_pag_libre;
- // static t_list *  tabla_hueco;
-
 void iniciar_tabla_paginacion(){
     list_tablas_paginacion = list_create();
 }
